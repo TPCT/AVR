530981c692303d56d69a2fc3b0e47d40e2d46ba9
TPCT <eslamjackie@outlook.com>
2019-08-31 18:32:49 -0700

    Create stdTypes.h

---
+++ AVR/stdTypes.h
@@ -0,0 +1,18 @@
+#ifndef STDTYPES_H
+#define STDTYPES_H
+typedef float FLOAT32;
+typedef double FLOAT64;
+typedef long double FLOAT128;
+typedef unsigned char bool;
+typedef char INT8;
+typedef unsigned char UINT8;
+typedef short INT16;
+typedef unsigned short UINT16;
+typedef long int INT32;
+typedef unsigned long int UINT32;
+typedef long long int INT64;
+typedef unsigned long long int UINT64;
+
+#define TRUE  1 
+#define FALSE 0
+#endif
